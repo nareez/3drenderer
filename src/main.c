a0c42bbe4b35ee84abddda24a63cdbbf5a8c0ee4
nareez <lugu.almeida@gmail.com>
2022-02-27 11:09:21 -0300

    colored faces

--- src/main.c
+++ src/main.c
@@ -36,7 +36,8 @@ void setup(void){
     );
 
     // load the cube values in the mesh data structure
-    load_obj_file_data("./assets/cube.obj");
+    // load_obj_file_data("./assets/cube.obj");
+    load_cube_mesh_data();
 }
 
 void process_input(void){ 
@@ -112,9 +113,6 @@ void update(void){
         face_vertices[0] = mesh.vertices[mesh_face.a - 1];
         face_vertices[1] = mesh.vertices[mesh_face.b - 1];
         face_vertices[2] = mesh.vertices[mesh_face.c - 1];
-
-
-
         vec3_t transformed_vertices[3];
 
         for (int j = 0; j < 3; j++) {
@@ -160,19 +158,26 @@ void update(void){
         }
 
         // loop all tree vertices to perform projection
-        triangle_t projected_triangle;
+        vec2_t projected_points[3];
         for (int j = 0; j < 3; j++) {
             // project current vertex
-            vec2_t projected_point = project(transformed_vertices[j]);
+            projected_points[j] = project(transformed_vertices[j]);
 
             // scale and translate to the middle of the screen
-            projected_point.x += (window_width / 2);
-            projected_point.y += (window_height / 2);
-
-            projected_triangle.points[j] = projected_point;
+            projected_points[j].x += (window_width / 2);
+            projected_points[j].y += (window_height / 2);
         }
-        array_push(triangles_to_render, projected_triangle);
+
+        triangle_t projected_triangle = {
+            .points = {
+                { projected_points[0].x, projected_points[0].y },
+                { projected_points[1].x, projected_points[1].y },
+                { projected_points[2].x, projected_points[2].y },
+            },
+            .color = mesh_face.color
+        };
         
+        array_push(triangles_to_render, projected_triangle);
     }
 }
 
@@ -188,13 +193,13 @@ void render(void){
             draw_filled_triangle(triangle.points[0].x, triangle.points[0].y
                         ,triangle.points[1].x, triangle.points[1].y
                         ,triangle.points[2].x, triangle.points[2].y
-                        , 0xffffffff);
+                        , triangle.color);
         }
         if (render_method == RENDER_FILL_TRIANGLE_WIRE || render_method == RENDER_WIRE || render_method == RENDER_WIRE_VERTEX){
             draw_triangle(triangle.points[0].x, triangle.points[0].y
                     ,triangle.points[1].x, triangle.points[1].y
                     ,triangle.points[2].x, triangle.points[2].y
-                    , 0x000000ff);
+                    , 0xffffffff);
         }
         if (render_method == RENDER_WIRE_VERTEX){
             draw_rect(triangle.points[0].x - 3, triangle.points[0].y - 3, 6, 6, 0x00ff0000);
@@ -202,8 +207,6 @@ void render(void){
             draw_rect(triangle.points[2].x - 3, triangle.points[2].y - 3, 6, 6, 0x00ff0000);
         }
     }
-
-    // draw_filled_triangle(300, 100, 50, 400, 500, 700, 0xFF00FF00);
 
     //clear array of triangles to render
     array_free(triangles_to_render);
